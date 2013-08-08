61804db98b3d480e98f778845cddc789cb65b3df
PRADET Quentin <quentin.pradet@cea.fr>
2013-08-08 09:34:17 +0200

    Sort translations using a template tag

--- syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
+++ syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
@@ -9,4 +9,6 @@ register = template.Library()
 def is_removed(value, arg):
     return value.filter(removed=arg)
 
-    
+@register.filter
+def sort_translations(value):
+    return value.order_by('category', 'verb')    
