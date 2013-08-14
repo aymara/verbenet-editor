b7d716b007c6658c4546c100873d506da9fd57ec
Quentin Pradet (@alwaysdata) <quentin.pradet@gmail.com>
2013-08-14 22:49:32 +0200

    Automatic versioning of static files

--- syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
+++ syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
@@ -12,3 +12,22 @@ def is_removed(value, arg):
 @register.filter
 def sort_translations(value):
     return sorted(list(value))
+
+from django.conf import settings
+import os, re
+
+rx = re.compile(r"^(.*)\.(.*?)$")
+
+@register.simple_tag
+def version(path):
+    full_path = os.path.join(settings.STATIC_ROOT, path)
+    try:
+        # Get file modification time.
+        os.stat_float_times(False)
+        mtime = os.path.getmtime(full_path)
+        changed = rx.sub(r"\1.%d.\2" % mtime, path)
+    except OSError:
+        # Returns normal url if this file was not found in filesystem.
+        changed = '%s%s' % (settings.STATIC_URL, path)
+    # Cheats, but adding "static" is just what the static tag does anyway...
+    return os.path.join('/static', changed)
