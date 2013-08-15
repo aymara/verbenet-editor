b41e6d2f48099a861f33d86056b868e1371da6ed
Quentin Pradet <quentin.pradet@gmail.com>
2013-08-15 10:49:10 +0200

    Use XML order to reorder position instead of implementation-dependant hack

--- syntacticframes_project/syntacticframes/management/commands/add_order_info.py
+++ syntacticframes_project/syntacticframes/management/commands/add_order_info.py
@@ -17,13 +17,28 @@ def iprint(indent, stuff):
 def update_class(xml_frameset, db_frameset, parent=None, indent=0):
     iprint(indent, db_frameset.name)
 
-    position = 1
-    for xml_f, db_f in zip(xml_frameset['frames'],
-                           db_frameset.verbnetframe_set.all()):
-        db_f.position = position
-        db_f.save()
-        position += 1
-        iprint(indent, "{} vs {}".format(db_f.syntax, xml_f.structure))
+    positions, current_position = {}, 1
+    xml_frames = xml_frameset['frames']
+    db_frames = db_frameset.verbnetframe_set.all()
+
+    for xml_f in xml_frames:
+        iprint(indent, "{} -> {}".format((xml_f.structure, xml_f.semantics, xml_f.roles), current_position))
+        positions[xml_f.structure, xml_f.semantics, xml_f.roles] = current_position
+        current_position += 1
+
+    for db_f in db_frames:
+        frame_tuple = db_f.syntax, db_f.semantics, db_f.roles_syntax
+        iprint(indent, "Let's see {}".format(frame_tuple))
+
+        try:
+            correct_position = positions[frame_tuple]
+            iprint(indent, "Position for {} is {}".format(db_f.syntax, correct_position))
+            db_f.position = correct_position
+            db_f.save()
+        except KeyError:
+            iprint(indent, "Unknown frame {}".format(frame_tuple))
+        except:
+            iprint(indent, "Other error for frame {}".format(frame_tuple))
 
     for xml_children, db_children in zip(xml_frameset['children'],
                                          db_frameset.get_children()):
