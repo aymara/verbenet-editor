503eb11b0951f64d5f8813a2b5b7b5834b2a9989
Quentin Pradet <quentin.pradet@gmail.com>
2013-08-23 15:35:11 +0200

    Complicated classes sorter

---
+++ syntacticframes_project/syntacticframes/management/commands/complicated_classes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter
+
+from django.core.management.base import BaseCommand, CommandError
+
+from syntacticframes.models import LevinClass, VerbNetClass, VerbNetFrameSet, VerbNetFrame
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        numbers = []
+        for lc in LevinClass.objects.all():
+            local_number = 0
+            for vn in lc.verbnetclass_set.all():
+                for fs in vn.verbnetframeset_set.all():
+                    local_number += len(fs.verbnetframe_set.all())
+                
+            numbers.append(("{}: {}".format(lc.number, lc.name), local_number))
+
+        numbers.sort(key=itemgetter(1), reverse=True)
+        for name, number in numbers:
+            print(name, number)
