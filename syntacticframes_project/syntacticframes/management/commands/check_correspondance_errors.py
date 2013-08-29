f285843f1b554fd2225ee324c4b97faabb7b5165
Quentin Pradet (@alwaysdata) <quentin.pradet@gmail.com>
2013-08-29 08:43:36 +0200

    Show errors with correspondances, not between algorithms

--- syntacticframes_project/syntacticframes/management/commands/check_correspondance_errors.py
+++ syntacticframes_project/syntacticframes/management/commands/check_correspondance_errors.py
@@ -8,17 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for vn_class in VerbNetClass.objects.all():
             try:
-                old = mapping.parse_complex_lvfladl(vn_class.ladl_string)
-                new = parse.get_ladl_list(vn_class.ladl_string)
-                if old != new:
-                    print("Difference for {} in {}! {} <-> {}".format(vn_class.ladl_string, vn_class.name, old, new))
+                parse.get_ladl_list(vn_class.ladl_string)
             except parse.UnknownClassException as e:
                 print('{:<30} {}'.format(vn_class.name, e))
 
             try:
-                old = mapping.parse_complex_lvfladl(vn_class.lvf_string)
-                new = parse.get_lvf_list(vn_class.lvf_string)
-                if old != new:
-                    print("Difference for {} in {}! {} <-> {}".format(vn_class.lvf_string, vn_class.name, old, new))
+                parse.get_lvf_list(vn_class.lvf_string)
             except parse.UnknownClassException as e:
                 print('{:<30} {}'.format(vn_class.name, e))
