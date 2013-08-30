c902394258e686831803eb1de02c4db2db2b2d2f
Quentin Pradet <quentin.pradet@gmail.com>
2013-08-30 10:02:12 +0200

    Move update_all_verbs to a command
    Also ignore wrong LVF/LADL mappings.

--- syntacticframes_project/syntacticframes/management/commands/loadverbnet.py
+++ syntacticframes_project/syntacticframes/management/commands/loadverbnet.py
@@ -22,19 +22,6 @@ def iprint(indent, stuff):
     pass
     #print(" " * indent, stuff)
 
-def update_all_verbs():
-    "Updates all verb translations is something went wrong below."
-    when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-    verb_logger.info("{}: Start full update of verb translations".format(when))
-    for db_vnclass in VerbNetClass.objects.all():
-        print(db_vnclass.name)
-        db_rootframeset = db_vnclass.verbnetframeset_set.get(parent=None)
-        update_verbs(db_rootframeset,
-                     db_rootframeset.ladl_string,
-                     db_rootframeset.lvf_string)
-    when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-    verb_logger.info("{}: Ended full update of verb translations".format(when))
-
 def update_verbs(db_frameset, current_ladl, current_lvf):
     verbs = VerbTranslation.objects.filter(frameset=db_frameset)
     initial_set = {(v.verb, v.category) for v in verbs}
---
+++ syntacticframes_project/syntacticframes/management/commands/update_all_verbs.py
@@ -0,0 +1,30 @@
+import logging
+from time import gmtime, strftime
+
+from django.core.management.base import BaseCommand
+
+from parsecorrespondance import parse
+from syntacticframes.models import VerbNetClass
+from .loadverbnet import update_verbs
+
+verb_logger = logging.getLogger('verbs')
+
+def update_all_verbs():
+    "Updates all verb translations is something went wrong below."
+    when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+    verb_logger.info("{}: Start full update of verb translations".format(when))
+    for db_vnclass in VerbNetClass.objects.all():
+        print(db_vnclass.name)
+        db_rootframeset = db_vnclass.verbnetframeset_set.get(parent=None)
+        try:
+            update_verbs(db_rootframeset,
+                         db_rootframeset.ladl_string,
+                         db_rootframeset.lvf_string)
+        except parse.UnknownClassException as e:
+            print("Ignoring {}".format(e))
+    when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+    verb_logger.info("{}: Ended full update of verb translations".format(when))
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        update_all_verbs()
