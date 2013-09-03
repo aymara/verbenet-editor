a6f3758d00ae71afae4460f93770c102bf9ec131
Quentin Pradet (@alwaysdata) <quentin.pradet@gmail.com>
2013-09-03 19:12:52 +0200

    PEP8: two empty lines

--- syntacticframes_project/syntacticframes/management/commands/update_all_verbs.py
+++ syntacticframes_project/syntacticframes/management/commands/update_all_verbs.py
@@ -7,7 +7,9 @@ from parsecorrespondance import parse
 from syntacticframes.models import VerbNetClass
 from .loadverbnet import update_verbs
 
+
 verb_logger = logging.getLogger('verbs')
+
 
 def update_all_verbs():
     "Updates all verb translations is something went wrong below."
@@ -24,6 +26,7 @@ def update_all_verbs():
             print("Ignoring {}".format(e))
     when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
     verb_logger.info("{}: Ended full update of verb translations".format(when))
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
