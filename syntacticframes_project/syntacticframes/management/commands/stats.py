038340cc3381c9066fd3d54c688b539390f878d7
Quentin Pradet (@alwaysdata) <quentin.pradet@gmail.com>
2013-09-23 15:04:44 +0200

    Compare French/English classes/verbs in stats

--- syntacticframes_project/syntacticframes/management/commands/stats.py
+++ syntacticframes_project/syntacticframes/management/commands/stats.py
@@ -7,13 +7,19 @@ from syntacticframes.models import VerbNetClass, VerbTranslation
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        all_verbs = set()
-        num_classes, num_verbs = 0, 0
+        all_verbs, all_members = set(), set()
+        num_classes, num_verbs, num_members = 0, 0, 0
         for vn_class in VerbNetClass.objects.all():
             num_classes += 1
-            for t in VerbTranslation.objects.filter(verbnet_class__exact = vn_class,
-                                                    category = 'both'):
-               num_verbs += 1
-               all_verbs.add(t.verb) 
+            for vn_fs in vn_class.verbnetframeset_set.all():
+                for t in VerbTranslation.objects.filter(frameset=vn_fs,
+                                                        category = 'both'):
+                   all_verbs.add(t.verb) 
+                   num_verbs += 1
+                for m in vn_fs.verbnetmember_set.all():
+                    all_members.add(m.lemma)
+                    num_members += 1
 
-        print("{} classes, {} verbs/class".format(num_classes, num_verbs / num_classes))
+        print("{} verbs ({} unique)".format(num_verbs, len(all_verbs)))
+        print("{} members ({} unique)".format(num_members, len(all_members)))
+        print("{} classes, {} verbs/class, {} members/class".format(num_classes, num_verbs / num_classes, num_members/num_classes))
