67540cc7b04a6fe8cdb4fb58d9918576aa94a0a5
Quentin Pradet <quentin.pradet@gmail.com>
2014-03-26 14:07:33 +0100

    Reindent inedit plugin

--- syntacticframes_project/assets/js/jquery.inedit.js
+++ syntacticframes_project/assets/js/jquery.inedit.js
@@ -1,123 +1,125 @@
 /*
- *	jQuery inedit version 1.04
+ * jQuery inedit version 1.04
  *
- *	Date: 15/09/2012
- *	Author: Zeamy Team
- *	Contact: contact@zeamy.com
- *	Website: http://www.webolitic.com/inedit
- *	
- *	Copyright (c) 2012 webolitic.com
- * 	http://www.opensource.org/licenses/mit-license.php
+ * Date: 15/09/2012
+ * Author: Zeamy Team
+ * Contact: contact@zeamy.com
+ * Website: http://www.webolitic.com/inedit
  *
+ * Copyright (c) 2012 webolitic.com
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Date: 26/03/2014
+ * Updates by Quentin Pradet
  */
 
 (function(jQuery){
 
-	// selectors
-	var event = {
-		click: 'click',
-		blur: 'blur',
-		focus: 'focus',
-		keypress: 'keypress'
-	};
+    // selectors
+    var event = {
+        click: 'click',
+        blur: 'blur',
+        focus: 'focus',
+        keypress: 'keypress'
+    };
 
-	// inedit function
-	jQuery.fn.inedit = function(opts){
+    // inedit function
+    jQuery.fn.inedit = function(opts){
 
-		// variables
-		var selection 	 = this;
-		var types 		 = ['input', 'textarea'];
-		var positions	 = ['before', 'after', 'in'];
-		var options		 = jQuery.extend({},jQuery.fn.inedit.options, opts);
-		var indexType	 = jQuery.inArray(options.type, types);
-		var indexPos	 = jQuery.inArray(options.position, positions);
+        // variables
+        var selection  = this;
+        var types      = ['input', 'textarea'];
+        var positions  = ['before', 'after', 'in'];
+        var options    = jQuery.extend({},jQuery.fn.inedit.options, opts);
+        var indexType  = jQuery.inArray(options.type, types);
+        var indexPos   = jQuery.inArray(options.position, positions);
 
-		
-		// check if type is correct
-		if (indexType < 0){
-			console.log('Unknown type "' + options.type + '"');
-			return this;
-		}
 
-		// check if position is correct
-		if (indexPos < 0){
-			console.log('Unknown type "' + options.position + '"');
-			return this;
-		}
+        // check if type is correct
+        if (indexType < 0){
+            console.log('Unknown type "' + options.type + '"');
+            return this;
+        }
 
-		var styleClassName 	= "";
-		var styleObject		= null;
+        // check if position is correct
+        if (indexPos < 0){
+            console.log('Unknown type "' + options.position + '"');
+            return this;
+        }
 
-		// parse a style argument
-		if ((typeof options.style == 'string') && (jQuery.trim(options.style) != "")){
-			styleClassName = 'class="' + options.style + '"';
-		}
+        var styleClassName  = "";
+        var styleObject     = null;
 
-		if (typeof options.style == 'object') {
-			styleObject = options.style;
-		}
+        // parse a style argument
+        if ((typeof options.style == 'string') && (jQuery.trim(options.style) != "")){
+            styleClassName = 'class="' + options.style + '"';
+        }
 
-		// define input elements
-		var inputElement = new Array();
-		inputElement[0]	 = '<input 	  style="display:none" ' + styleClassName + ' value="" type="text" />';
-		inputElement[1]  = '<textarea style="display:none" ' + styleClassName + ' > </textarea>';
+        if (typeof options.style == 'object') {
+            styleObject = options.style;
+        }
 
-		options.clickStarted = false;
+        // define input elements
+        var inputElement = new Array();
+        inputElement[0] = '<input    style="display:none" ' + styleClassName + ' value="" type="text" />';
+        inputElement[1] = '<textarea style="display:none" ' + styleClassName + ' > </textarea>';
 
-		// for each selected field do this
-		jQuery(selection).each(function(){
-			var selected = this;
+        options.clickStarted = false;
 
-			// on click function
-			jQuery(selected).bind(event.click, function(){
-				var text;
-				var data_text = jQuery(selected).data('text');
+        // for each selected field do this
+        jQuery(selection).each(function(){
+            var selected = this;
 
-				// prevent to start handler multiple times
-				if (options.clickStarted == true) return;
+            // on click function
+            jQuery(selected).bind(event.click, function(){
+                var text;
+                var data_text = jQuery(selected).data('text');
 
-				options.clickStarted = true;
+                // prevent to start handler multiple times
+                if (options.clickStarted == true) return;
 
-				// fetch the text to edit
-				if (data_text) {
-					text = data_text;
-				} else {
-					text = jQuery(selected).text();
-				}
+                options.clickStarted = true;
 
-				// if text contains empty_text, start with empty string
-				if (options.empty_text == text) {
-					text = '';
-				}
+                // fetch the text to edit
+                if (data_text) {
+                    text = data_text;
+                } else {
+                    text = jQuery(selected).text();
+                }
 
-				var input = null;
+                // if text contains empty_text, start with empty string
+                if (options.empty_text == text) {
+                    text = '';
+                }
 
-				// remove text from text label
-				jQuery(selected).text("");
+                var input = null;
 
-				// append input element after selected dom element
-				switch (options.position){
-					case 'after':
-						jQuery(selected).after(inputElement[indexType]);
-						input = jQuery(selected).next();
-						break;
-					case 'before':
-						jQuery(selected).before(inputElement[indexType]);
-						input = jQuery(selected).prev();
-						break;
-					case 'in':
-						jQuery(selected).append(inputElement[indexType]);
-						input = jQuery(selected).find(options.type);
-						break;
-				}
+                // remove text from text label
+                jQuery(selected).text("");
 
-				// save data on Enter
-				jQuery(input).bind(event.keypress, function(e) {
-					var code = (e.keyCode ? e.keyCode : e.which);
-					if(code == 13) { 
+                // append input element after selected dom element
+                switch (options.position){
+                    case 'after':
+                        jQuery(selected).after(inputElement[indexType]);
+                        input = jQuery(selected).next();
+                        break;
+                    case 'before':
+                        jQuery(selected).before(inputElement[indexType]);
+                        input = jQuery(selected).prev();
+                        break;
+                    case 'in':
+                        jQuery(selected).append(inputElement[indexType]);
+                        input = jQuery(selected).find(options.type);
+                        break;
+                }
+
+                // save data on Enter
+                jQuery(input).bind(event.keypress, function(e) {
+                    var code = (e.keyCode ? e.keyCode : e.which);
+                    if(code == 13) {
                         // enable on click event
                         options.clickStarted = false;
-                        
+
                         // execute on blur method
                         var result = options.onEnd(input, selected);
                         if((typeof result === 'undefined') || (result == true)) {
@@ -129,39 +131,39 @@
                             jQuery(selected).show();
                             jQuery(input).remove();
                         }
-					}
-				});
-				
-				// @ focus call onStart functions
-				jQuery(input).bind(event.focus, function() {
-					// execute on focus method
-					options.onStart(input, selected);
-				});
+                    }
+                });
 
-				// assign style from given object to input field
-				if (styleObject != null){
-					for (s in styleObject){
-						jQuery(input).css(s, styleObject[s]);
-					}
-				}
+                // @ focus call onStart functions
+                jQuery(input).bind(event.focus, function() {
+                    // execute on focus method
+                    options.onStart(input, selected);
+                });
 
-				jQuery(input).val(text);
-				jQuery(input).show();	
-				jQuery(input).focus();
-			});
-		});
-		
-		return this;
-	}
+                // assign style from given object to input field
+                if (styleObject != null){
+                    for (s in styleObject){
+                        jQuery(input).css(s, styleObject[s]);
+                    }
+                }
 
-	// inedit options 
-	jQuery.fn.inedit.options = {
-		type: 'input',
-		onStart: jQuery.noop,
-		onEnd: jQuery.noop,
-		style: "",
-		position: 'after',
-		empty_text: '',
-	};
+                jQuery(input).val(text);
+                jQuery(input).show();
+                jQuery(input).focus();
+            });
+        });
+
+        return this;
+    }
+
+    // inedit options
+    jQuery.fn.inedit.options = {
+        type: 'input',
+        onStart: jQuery.noop,
+        onEnd: jQuery.noop,
+        style: "",
+        position: 'after',
+        empty_text: '',
+    };
 
 })(jQuery);
