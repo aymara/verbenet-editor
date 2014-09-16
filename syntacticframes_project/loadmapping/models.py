04088fc45de9901dffb75bb1d706cf3ef1f94c44
Quentin Pradet <quentin.pradet@gmail.com>
2014-09-16 11:30:42 +0200

    Clarify LVFVerb.construction field comment

--- syntacticframes_project/loadmapping/models.py
+++ syntacticframes_project/loadmapping/models.py
@@ -5,7 +5,9 @@ class LVFVerb(models.Model):
     sense = models.PositiveSmallIntegerField()
 
     lvf_class = models.CharField(max_length=10)
-    # No encoding is contained into another
+    # No encoding is contained into another, so using a simple TextField for
+    # constructions instead of an enum is fine. We can search for 'T14b0', it
+    # will work even if the constructions also contains 'T1300'.
     construction = models.TextField()
 
     def __str__(self):
