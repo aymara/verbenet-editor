1901cef4be40d25b971730c062cfcd7c6957ee29
Quentin Pradet <quentin.pradet@gmail.com>
2014-09-16 11:27:37 +0200

    Import LVF in a Django migration to fix LVF tests

---
+++ syntacticframes_project/loadmapping/migrations/0002_auto_20140916_1053.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+from os.path import join
+
+from django.db import models, migrations
+from django.conf import settings
+
+from loadmapping.models import LVFVerb
+
+def import_verbs(apps, schema_editor):
+    LVFVerb = apps.get_model('loadmapping', 'LVFVerb')
+    with open(join(settings.SITE_ROOT, 'loadmapping/fixtures/lvfverb.json')) as fixture:
+            for entry in json.loads(fixture.read()):
+                assert entry['model'] == 'loadmapping.lvfverb'
+                fields = entry['fields']
+                LVFVerb(
+                    lemma=fields['lemma'],
+                    sense=fields['sense'],
+                    lvf_class=fields['lvf_class'],
+                    construction=fields['construction']).save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('loadmapping', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(import_verbs)
+    ]
