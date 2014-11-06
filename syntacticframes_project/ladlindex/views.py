fb1dae79fa4672714946beccf241204b4f7a6677
Quentin Pradet <quentin.pradet@gmail.com>
2014-11-06 21:34:00 +0100

    Sort and link classes in reversed LADL index

--- syntacticframes_project/ladlindex/views.py
+++ syntacticframes_project/ladlindex/views.py
@@ -1,4 +1,6 @@
 import re
+from distutils.version import LooseVersion
+from collections import defaultdict, OrderedDict
 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -34,19 +36,29 @@ def ladl_sort_key(ladl):
 
     return digit1, letter, digit2
 
+def vn_sort_key(vn):
+    return LooseVersion(vn)
+
 def index(request):
-    inversed_ladl = {ladl: [] for ladl in parse.ladl_list if not ladl.startswith('C_')}
-    for fs in VerbNetFrameSet.objects.all():
+    inversed_ladl = {ladl: defaultdict(list) for ladl in parse.ladl_list if not ladl.startswith('C_')}
+
+    for fs in VerbNetFrameSet.objects.select_related('verbnet_class', 'verbnet_class__levin_class').all():
         ladl_list = get_ladl_classes(parse.FrenchMapping('LADL', fs.ladl_string).parse_tree)
         for ladl in ladl_list:
-            inversed_ladl[ladl].append(fs.name)
+            levin_number = fs.verbnet_class.levin_class.number
+            inversed_ladl[ladl][levin_number].append(fs.name)
 
-    ladl_sorted_keys = sorted(inversed_ladl.keys(), key=ladl_sort_key)
-    inversed_ladl_pairs = [(key, inversed_ladl[key]) for key in ladl_sorted_keys]
+    # Sort LADL tables, Levin groups, and framesets
+    for key in inversed_ladl:
+        for group in inversed_ladl[key]:
+            inversed_ladl[key][group] = sorted(inversed_ladl[key][group], key=vn_sort_key)
+        inversed_ladl[key] = OrderedDict(sorted(inversed_ladl[key].items(), key=lambda kv: vn_sort_key(kv[0])))
+
+    inversed_ladl = OrderedDict(sorted(inversed_ladl.items(), key=lambda kv: ladl_sort_key(kv[0])))
 
     template = loader.get_template('ladl_index.html')
     context = RequestContext(request, {
-        'inversed_ladl_pairs': inversed_ladl_pairs,
+        'inversed_ladl': inversed_ladl,
     })
 
     return HttpResponse(template.render(context))
