7ed4c7e24a319b43810781a92f70e113fea82adc
Quentin Pradet <quentin.pradet@gmail.com>
2014-11-19 10:07:45 +0100

    Use format() instead of %

--- syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
+++ syntacticframes_project/syntacticframes/templatetags/syntacticframes_extras.py
@@ -18,7 +18,7 @@ def version(path):
         # Get file modification time.
         os.stat_float_times(False)
         mtime = os.path.getmtime(full_path)  # raises OSError if file does not exist
-        path = rx.sub(r"\1.%d.\2" % mtime, path)
+        path = rx.sub(r"\1.{}.\2".format(mtime), path)
 
     return os.path.join(settings.STATIC_URL, path)
 
