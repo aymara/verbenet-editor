0a3a9aef6de281ecda98ac38321ec890511035e1
Quentin Pradet <quentin.pradet@gmail.com>
2015-01-30 15:15:28 +0100

    Show definition of class in verbes-html

--- syntacticframes_project/resources/management/commands/renderladl.py
+++ syntacticframes_project/resources/management/commands/renderladl.py
@@ -11,10 +11,29 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.html import strip_spaces_between_tags as minify_html
 
+def formules_definitoires():
+    with open(join(settings.SITE_ROOT, 'resources/tables-3.4/tdt-verbes/formules-definitoires.csv')) as tdtf:
+        tdt = csv.reader(tdtf, delimiter=';', quotechar='"')
+        next(tdt)
+
+        formules = {}
+        for table in tdt:
+            if not table[5]:
+                continue
+
+            table_name = table[5]
+            construction = table[0][1:-1]
+            conjonction = table[1]
+            formules[table_name] = {'construction': construction, 'conjonction': conjonction}
+
+        return formules
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         env = Environment(loader=FileSystemLoader(join(settings.SITE_ROOT, 'templates/')), autoescape=True, trim_blocks=True)
         template = env.get_template('table.html')
+
+        formules = formules_definitoires()
 
         for f in (glob(join(settings.SITE_ROOT, 'resources/tables-3.4/verbes/*.csv')) +
                 glob(join(settings.SITE_ROOT, 'resources/tables-3.4/figees/*.csv'))):
@@ -25,5 +44,12 @@ class Command(BaseCommand):
             with open(f) as csvf, open(join(settings.SITE_ROOT, 'assets/verbes-html/{}.lgt.html'.format(classe)), 'w') as html:
                 ladlreader = csv.reader(csvf, delimiter=';', quotechar='"')
 
-                context = {'classe': stripped_class, 'header': next(ladlreader), 'verb_lines': ladlreader, 'toalign': ['+', '~', '-', '?', '<E>']}
+                context = {
+                    'classe': stripped_class,
+                    'formule': formules.get(stripped_class),
+                    'header': next(ladlreader),
+                    'verb_lines': ladlreader,
+                    'toalign': ['+', '~', '-', '?', '<E>']
+                }
+
                 html.write(minify_html(template.render(context)))
