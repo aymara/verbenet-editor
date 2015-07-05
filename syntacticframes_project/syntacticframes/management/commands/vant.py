8a2db8ae0006c1a16a0d1a9c100ce23d433c7897
Quentin Pradet <quentin.pradet@gmail.com>
2015-07-05 10:49:49 +0200

    Update Vant -> V-ant as for Vinf

--- syntacticframes_project/syntacticframes/management/commands/vant.py
+++ syntacticframes_project/syntacticframes/management/commands/vant.py
@@ -13,16 +13,16 @@ class Command(BaseCommand):
 
         verb_logger = logging.getLogger('verbs')
         when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-        verb_logger.info("{}: Started Vinf update.".format(when))
+        verb_logger.info("{}: Started Vant update.".format(when))
 
         try:
             with transaction.atomic():
                 for vn_class in VerbNetClass.objects.all():
                     for frameset in vn_class.verbnetframeset_set.all():
                         for frame in frameset.verbnetframe_set.all():
-                            if 'Vinf' in frame.roles_syntax or 'Vinf' in frame.syntax:
-                                frame.roles_syntax = frame.roles_syntax.replace('Vinf', 'V-inf')
-                                frame.syntax = frame.syntax.replace('Vinf', 'V-inf')
+                            if 'Vant' in frame.roles_syntax or 'Vant' in frame.syntax:
+                                frame.roles_syntax = frame.roles_syntax.replace('Vant', 'V-ant')
+                                frame.syntax = frame.syntax.replace('Vant', 'V-ant')
                                 frame.save()
                                 when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
                                 verb_logger.info("{}: {}, {}: {}".format(when, vn_class.name, frameset.name, frame))
@@ -33,5 +33,5 @@ class Command(BaseCommand):
             verb_logger.info("{}: Exception, everything was backed out.".format(when))
         else:
             when = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-            verb_logger.info("{}: Ended Vinf update.".format(when))
+            verb_logger.info("{}: Ended Vant update.".format(when))
         verb_logger.info('')
