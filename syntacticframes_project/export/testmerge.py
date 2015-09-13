291e7d26cb6a0fc6de8036e224754600062f0c00
Quentin Pradet <quentin.pradet@gmail.com>
2015-09-13 14:20:48 +0400

    Remove irrelevant parts in test

--- syntacticframes_project/export/testmerge.py
+++ syntacticframes_project/export/testmerge.py
@@ -288,15 +288,13 @@ class TestExport(SimpleTestCase):
     def test_vinf(self):
         self.maxDiff = None
         new_syntax = merge_primary_and_syntax(
-            'NP V PP.source de V-inf',
-            'Pivot V {pour/de la part de} Source Theme<+de VSource-inf>')
+            'NP V de V-inf',
+            'Pivot V Theme<+de VSource-inf>')
         xml = xml_of_syntax(new_syntax)
         self.assertEqual(
             ET.tostring(xml, encoding='unicode'),
             '<SYNTAX>'
             '<NP value="Pivot"><SYNRESTRS /></NP>'
             '<VERB />'
-            '<PREP><SELRESTRS><SELRESTR Value="de la part de;pour" /></SELRESTRS></PREP>'
-            '<NP value="Source"><SYNRESTRS /></NP>'
             '<VINF emptysubjectrole="Source" introduced_by="de" is_true_prep="false" value="Theme" />'
             '</SYNTAX>')
